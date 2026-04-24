2db25b54d298af93eda2a3a6b9ac8ab65717213a
ssbg04 <crischarlesgarcia345@gmail.com>
2026-04-24 11:19:10 +0800

    remove uploads from tracking

--- testing/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<!-- <script src="assets/js/app.js"></script> -->
-<script src="assets/js/main.js"></script>
-<script src="assets/js/auth.js"></script>
-<script src="assets/js/inventory.js"></script>
-<script src="assets/js/vendors.js"></script>
-<script src="assets/js/po.js"></script>
-<script src="assets/js/analytics.js"></script>
