1065bd06f7752956e675fc7378b5d0ce3d4046fb
ssbg04 <crischarlesgarcia345@gmail.com>
2026-04-24 13:27:02 +0800

    new database creds

--- includes/db/db.php
+++ includes/db/db.php
@@ -4,13 +4,15 @@
 // $db_name = 'icis'; // Change this to your database name
 // $username = 'root'; // Change if your DB user is different
 // $password = ''; // Change if your DB has a password
-$host = 'sql.freedb.tech';
-$db_name = 'freedb_icis-inventory'; // Change this to your database name
-$username = 'freedb_kristyarls'; // Change if your DB user is different
-$password = 'Wwh7Du4?MJ8wKg!'; // Change if your DB has a password
+// $port = 3306;
+$host = 'icis-inventory-crischarlesgarcia345-12a1.c.aivencloud.com';
+$db_name = 'defaultdb'; // Change this to your database name
+$username = 'webapp'; // Change if your DB user is different
+$password = 'AVNS_D5rqxPUXzPwYy2307kd'; // Change if your DB has a password
+$port = 22378;
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db_name", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
